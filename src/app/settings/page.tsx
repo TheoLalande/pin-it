99070b1106a051d8b36c104b1eb5b4328ddcaa8a
theo.lalande <theo.lalande@compufirst.com>
2024-09-26 16:04:31 +0200

    ajout d'un element aux settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -2,7 +2,7 @@ import ProfilePicture from '../components/settings/ProfilePicture'
 import SettingElement from '../components/settings/SettingElement'
 import { FaRegHeart, FaRegUser } from 'react-icons/fa6'
 import { RiSettings2Line } from 'react-icons/ri'
-
+import { FaRegMap, FaRegLightbulb } from 'react-icons/fa'
 type dataType = {
   icon: JSX.Element
   title: string
@@ -26,13 +26,22 @@ const page = () => {
       title: 'Préférences',
       description: "Paramètres de l'application",
     },
+    {
+      icon: <FaRegMap size={25} />,
+      title: 'Carte',
+      description: 'Paramètres de la carte',
+    },
+    {
+      icon: <FaRegLightbulb size={25} />,
+      title: 'Aide et suggestions',
+      description: 'À propos de l’application',
+    },
   ]
 
   return (
     <div className="w-screen h-screen bg-[#fafafa]">
       <div className="w-full  flex items-center justify-center flex-col ">
         <ProfilePicture imagePath="/assets/profilePicture.jpeg" />
-
         {data.map(d => (
           <SettingElement key={d.title} title={d.title} description={d.description} icon={d.icon} />
         ))}
