bcd17f8868f9dfc376eebb75a67fd55315c8b087
theo.lalande <theo.lalande@compufirst.com>
2024-09-27 16:21:06 +0200

    modification des ombres de card de cartes

--- src/app/components/settings/maps/MapStyleElt.tsx
+++ src/app/components/settings/maps/MapStyleElt.tsx
@@ -9,9 +9,9 @@ type propsType = {
 
 const MapStyleElt = (props: propsType) => {
   return (
-    <button className="mr-10 flex w-full  shadow-[0_3px_10px_rgb(0,0,0,0.2)] rounded-2xl p-5  items-center mb-3">
+    <button className="mr-10 flex w-full  shadow-md rounded-2xl p-5  items-center mb-3">
       <div className="w-1/2 h-full ">
-        <Image alt="terain" src={props.imagePath} width={130} height={150} className="rounded-3xl" />
+        <Image alt="terain" src={props.imagePath} width={130} height={150} className="rounded-3xl shadow-md" />
       </div>
       <div className="w-1/2 h-full  flex flex-col ml-5 text-left gap-3">
         <span className="text-black text-lg font-medium">{props.title}</span>
